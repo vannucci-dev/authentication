899b8b78c7ed8b03631028cdef9debff44c95d09
Rufus Blake <rufus.blake.uk@gmail.com>
2019-08-29 19:30:38 +0100

    add mongoose and create database to check user auth.

--- app.js
+++ app.js
@@ -2,6 +2,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true});
+const Schema = mongoose.Schema;
+userSchema = new Schema({email:String, password:String});
+
+const User = new mongoose.model('User', userSchema);
+
 
 const app = express();
 
@@ -18,6 +26,42 @@ app.get('/login', (req, res)=>{
 });
 app.get('/register', (req, res)=>{
   res.render('register');
+});
+app.get('/secrets', (req, res)=>{
+  res.render('secrets');
+});
+
+app.post('/register', (req, res)=>{
+
+  let username = req.body.username;
+  let password = req.body.password;
+
+  const user = new User({
+    email: username,
+    password:password
+  });
+  user.save();
+  res.redirect('/login');
+});
+
+app.post('/login', (req, res)=>{
+  let username = req.body.username;
+  let password = req.body.password;
+
+  User.findOne({email:username, password: password}, (err, result)=>{
+    if(!err){
+      if(!result){
+        console.log("Sorry no user found");
+        res.render('login');
+      }else{
+        if( result.email === username && result.password === password){
+          res.redirect('/secrets');
+        }
+      }
+    }else{
+      console.log(err);
+    }
+  });
 });
 
 app.listen(3000, ()=>{console.log("Server initiated @3000");});
